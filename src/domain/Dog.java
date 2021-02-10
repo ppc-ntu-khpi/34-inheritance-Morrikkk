e846d2a29374caa57450c5efd6d58ad9ce461903
Morrikkk <75643691+Morrikkk@users.noreply.github.com>
2021-02-10 15:30:47 +0200

    Update Dog.java

--- src/domain/Dog.java
+++ src/domain/Dog.java
@@ -1,50 +1,126 @@
 package domain;
 
+
+ /**
+ * The class Dog extends predator
+ */ 
 public class Dog extends Predator
 {
     private String kind;
 
-    public Dog(String name, int weight, String kind)
+
+
+/** 
+ *
+ * Dog
+ *
+ * @param name  the name
+ * @param weight  the weight
+ * @param kind  the kind
+ * @return public
+ */
+    public Dog(String name, int weight, String kind) 
     {
+
         this.name=name;
         this.weight=weight;
         this.kind=kind;
     }
 
-    public Dog()
+
+
+/** 
+ *
+ * Dog
+ *
+ * @return public
+ */
+    public Dog() 
     {
+
         this("Large", 15, "husky");
     }
 
-    public Dog(String name)
+
+
+/** 
+ *
+ * Dog
+ *
+ * @param name  the name
+ * @return public
+ */
+    public Dog(String name) 
     {
+
         this(name, 15, "husky");
     }
 
-    public void play()
+
+
+/** 
+ *
+ * Play
+ *
+ */
+    public void play() 
     {
+
         System.out.println("Dog is playing;");
     }
 
-    public void hunt()
+
+
+/** 
+ *
+ * Hunt
+ *
+ */
+    public void hunt() 
     {
+
         super.hunt();
         System.out.println("Dog hunting for the bone:");
     }
 
-    public String toString()
+
+
+/** 
+ *
+ * To string
+ *
+ * @return String
+ */
+    public String toString() 
     {
+
         return super.toString()+"\nkind:\t"+this.kind+"\n\nThis is Dog!";
     }
 
-    public void speak()
+
+
+/** 
+ *
+ * Speak
+ *
+ */
+    public void speak() 
     {
+
         super.speak();
         System.out.println("Woof! Woof!");
     }
 
-    public void eat()
+
+
+/** 
+ *
+ * Eat
+ *
+ */
+    public void eat() 
     {
+
         System.out.println("Dog eats bone and meal;");
     }
 }
